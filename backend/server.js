02334f2011c38eed2af4d1dc6f53a60583b5dd23
yashsinha501 <72601810+yashsinha501@users.noreply.github.com>
2023-12-18 14:17:11 +0530

    host config

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const app = express();
 const db = require('./config/database')
 const registerRoutes = require('./routes/registrationRoutes')
+const path = require('path')
 const PORT = process.env.PORT || 5000;
 
 // Middleware
@@ -12,6 +13,12 @@ app.use(cors())
 
 
 app.use('/api',registerRoutes)
+
+app.use(express.static(path.join(__dirname, '../frontend/dist')))
+
+app.get('*', function(req,res){
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
